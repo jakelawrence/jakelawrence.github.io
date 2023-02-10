9e26f7e2c9e2bd9a2241870f441900e6904177b5
Jake Lawrence <jake.lawrence.job.search@gmail.com>
2023-02-09 17:33:21 -0800

    Changed to HashRouter for GH Pages Support

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/ResumeNew";
-import { BrowserRouter as HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
